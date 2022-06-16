5f4640abf057a6e14c4cf68b60b32053d5d31a51
Carlangelomikhael <88195134+Carlangelomikhael@users.noreply.github.com>
2022-06-16 15:47:39 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,204 +1,122 @@
-import json
 import pickle
-import socket
-import os
-import argparse
-from hashlib import sha256
-import rsa
-import sqlite3
-
-# Parsing the node's username and password arguments passed in the CLI
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('name', metavar='N', type=str)
-parser.add_argument('password', metavar='N', type=str)
-args = parser.parse_args()
-vars_dict = vars(args)
-
-# Connecting to existing database
-conn = sqlite3.connect('test.db')
-
-# The cursor allow us to execute SQL commands
-c = conn.cursor()
 
 
-# Block Class with it's basic attributes
-# We have the block's index, transaction (between nodes) , timestamp (when it was created)
-# the hash of the previous block and the nonce (number used once)
-class Block:
-    def __init__(self, index, transaction, timestamp, previous_hash, nonce=0):
-        self.index = index
-        self.transaction = transaction
-        self.timestamp = timestamp
-        self.previous_hash = previous_hash
-        self.nonce = nonce
+class Client:
+    def __init__(self, database, minBufferSize, host, port, s, keysDir, wallet):
+        self.database = database
+        self.minBufferSize = minBufferSize
+        self.host = host
+        self.port = port
+        self.socket = s
+        self.keysDir = keysDir
+        self.wallet = wallet
 
-    # Function that take as an input all the block's attributes
-    # and outputs the hash of the block (encoded in sha256)
-    def compute_hash(self):
-        # json.dumps => Converts a Python object into a json string
-        # self.__dict__ => all attributes defined for the object Block
-        # json.dumps => Converts a Python object into a json string
-        # self.__dict__ => all attributes defined for the object Block
-        block_string = json.dumps(self.__dict__)
+    def start(self):
+        self.connect()
+        self.identify()
+        self.updateDatabase()
 
-        # encode() => Encode the json string
-        # sha256() => Hash it with sha256
-        # hexdigest() => Returns the encoded data in hexadecimal format
-        return (sha256(block_string.encode())).hexdigest()
+    def connect(self):
+        # Connecting to the server
+        print(f"[+] Connecting to {self.host}:{self.port}")
+        self.socket.connect((self.host, self.port))
+        print("[+] Connected.")
 
+    def identify(self):
+        length = str(len(self.wallet.address))
+        self.socket.send(self.toMinSize(length).encode())
+        self.socket.send(self.wallet.address.encode())
 
-# send/receive 4096 bytes each time
-BUFFER_SIZE = 4096
-SEPARATOR = "<SEPARATOR>"
+    def updateDatabase(self):
+        tableNames = ["Blocks", "Transactions", "Unconfirmed_Transactions", "UTXO"]
+        for tableName in tableNames:
+            if tableName == "Blocks" or tableName == "Transactions":
+                lastId = int(self.socket.recv(self.minBufferSize))
+                m = self.database.getLastObjectId(tableName)
+                self.socket.send(str(m).encode())
 
-# The ip address or hostname of the server, the receiver
-# If you are running the client script on another device use the ip address of the server
-host = socket.gethostbyname(socket.gethostname())
-# the port, let's use 5001
-port = 5001
+                for i in range(m + 1, lastId + 1):
+                    length = int(self.socket.recv(self.minBufferSize).decode().strip())
+                    object = pickle.loads(self.socket.recv(length))
+                    self.database.addObject(self.database.rawToObject(tableName, object), True)
+            else:
+                length = int(self.socket.recv(self.minBufferSize))
+                set1 = pickle.loads(self.socket.recv(length))
 
-# create the client socket
-s = socket.socket()
+                set2 = pickle.dumps(set(self.database.getObjectIdList(tableName)))
+                length = self.toMinSize(str(len(set2))).encode()
+                self.socket.send(length)
+                self.socket.send(set2)
 
-# Connecting to the server
-print(f"[+] Connecting to {host}:{port}")
-s.connect((host, port))
-print("[+] Connected.")
+                set2 = pickle.loads(set2)
+                # Elements that are missing
+                toAdd = set1 - set2
 
-# Getting the node's name and password from the CLI args
-name = vars_dict['name']
-password = vars_dict['password']
+                if toAdd:
+                    for i in range(0, len(toAdd)):
+                        length = int(self.socket.recv(self.minBufferSize))
+                        object = pickle.loads(self.socket.recv(length))
+                        self.database.addObject(object, definitive=True)
 
+                # Elements that are in excess
+                toDelete = set2 - set1
 
-def get_unconf_tx():
-    c.execute("SELECT * FROM Unconfirmed_Transactions LIMIT 1")
-    return c.fetchall()
+                if toDelete:
+                    for elmnt in toDelete:
+                        self.database.removeObject(self.database.getObjectById(tableName, elmnt[0]))
 
+    def transact(self, transactionSender, transactionReceiver, transactionAmount):
+        tx = self.wallet.constructTx(transactionSender, transactionReceiver, int(transactionAmount))
+        if tx is not None:
+            pickledTx = pickle.dumps(tx)
+            self.socket.send(self.toMinSize("2").encode())
 
-def login():
-    # Sending the node's username and password to the server
-    data_string = name + SEPARATOR + password
-    s.send(data_string.encode())
+            length = str(len(pickledTx))
+            self.socket.send(self.toMinSize(length).encode())
+            self.socket.send(pickledTx)
 
-    message = s.recv(BUFFER_SIZE).decode()
-    print(message)
-    if message == 'Logged in':
-        # update_blockchain_copy()
-        node_choice()
-
-
-def update_blockchain_copy():
-    message = s.recv(BUFFER_SIZE).decode()
-    if message == 'Your blockchain copy is not up to date please specify the path where the copy is saved':
-
-        print(message)
-
-        s.send('done'.encode())
-        message = s.recv(BUFFER_SIZE).decode()
-        print(message)
-
-    else:
-        print(message)
-
-
-def node_choice():
-    # Receiving the services message and printing it
-    # Taking the nodes choice
-    message = s.recv(BUFFER_SIZE).decode()
-    print(message)
-    choice = input()
-
-    try:
-        int(choice)
-
-        s.send(str(choice).encode())
-
-        if int(choice) == 1:
-            create_keys()
-        elif int(choice) == 2:
-            recv_block_data()
+            res = self.socket.recv(self.minBufferSize).strip().decode()
+            if res == "100":
+                self.database.addObject(tx)
+                for input in tx.inputs:
+                    self.database.removeObject(self.database.getUtxoByScript(input.lockingScript))
+            return True
         else:
-            close_connection()
-    except:
-        s.send(str(choice).encode())
-        error = s.recv(BUFFER_SIZE).decode()
-        print(error)
-        node_choice()
+            return None
 
+    def blockInfo(self):
+        self.socket.send(b"00001")
+        length = int(self.socket.recv(self.minBufferSize))
+        if length == 0:
+            return 0
+        else:
+            block = pickle.loads(self.socket.recv(length))
+            return block
 
-def create_keys():
-    response = s.recv(32).decode()
-    if response == '1':
-        # Generating a public and private key (1024 bits rsa keys are considered medium-strength keys)
-        (pubkey, privkey) = rsa.newkeys(1024)
+    def mine(self, block):
+        self.socket.send(b"00001")
+        block.mine(self.wallet)
+        length = str(len(pickle.dumps(block)))
+        self.socket.send(self.toMinSize(length).encode())
+        self.socket.send(pickle.dumps(block))
+        if int(self.socket.recv(self.minBufferSize).decode().strip()) == 100:
+            self.database.addObject(block)
+            for tx in block.transactions:
+                if tx.type == 2:
+                    self.database.addObject(tx)
+                    self.database.removeObject(self.database.getFirstObject("Unconfirmed_Transactions"))
+                    for output in tx.outputs:
+                        self.database.addObject(output)
+                elif tx.type == 1:
+                    self.database.addObject(tx)
+                    for output in tx.outputs:
+                        self.database.addObject(output)
 
-        # Setting the directory where the private key file will be saved
-        # if the directory doesn't exist we create it and we save the private key in bytes format
-        # The dumps() method of the Python pickle module serializes a python object
-        directory = f'C:\\Users\\User\\Downloads\\{name}_private_key'
-        filename = "PrivateKey.txt"
-        file_path = os.path.join(directory, filename)
-        if not os.path.isdir(directory):
-            os.mkdir(directory)
-        file = open(file_path, "wb")
-        file.write(pickle.dumps(privkey))
-        file.close()
+    def toMinSize(self, string):
+        while len(string) < self.minBufferSize:
+            string += " "
+        return string
 
-        # Setting the directory where the public key file will be saved
-        # same functionality as above
-        directory = f'C:\\Users\\User\\Downloads\\{name}_public_key'
-        filename = "PublicKey.txt"
-        file_path = os.path.join(directory, filename)
-        if not os.path.isdir(directory):
-            os.mkdir(directory)
-        file = open(file_path, "wb")
-        file.write(pickle.dumps(pubkey))
-        file.close()
-
-        send_public_key(file_path)
-    else:
-        print(response)
-        node_choice()
-
-
-def send_public_key(file_path):
-    # start sending the file
-    with open(file_path, "rb") as f:
-        while True:
-            # read the bytes from the file
-            bytes_read = f.read(BUFFER_SIZE)
-            if not bytes_read:
-                # file transmitting is done
-                break
-            # we use sendall to assure transmission in
-            # busy networks
-            s.sendall(bytes_read)
-
-
-def recv_block_data():
-    message = s.recv(BUFFER_SIZE).decode()
-    if message == 'No transactions to mine':
-        print(message)
-        node_choice()
-    else:
-        mine(message.split(SEPARATOR))
-
-
-def mine(block_list):
-    block_index, block_transaction, block_timestamp, block_previous_hash, block_nonce = block_list
-    block = Block(block_index, block_transaction, block_timestamp, block_previous_hash, int(block_nonce))
-    block_hash = block.compute_hash()
-
-    while not block_hash.startswith('0' * 5):
-        block.nonce += 1
-        block_hash = block.compute_hash()
-
-    s.send(block_hash.encode())
-    node_choice()
-
-
-def close_connection():
-    s.close()
-
-
-login()
+    def close(self):
+        self.socket.send(self.toMinSize("3").encode())
+        self.socket.close()
